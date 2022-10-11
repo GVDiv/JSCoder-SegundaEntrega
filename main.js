2a85e06f86c8674346458883fbfd625765968619
Gabriel Villamayor <gabitokun@outlook.com>
2022-10-11 15:09:05 -0300

    se agrego las demas opciones de cafe y cambio de imagenes

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function opcionSuaveDescafeinado(){
                 const galeriaCafeSuave2 = [
                     {
                         cafeSeleccionado: "Cafe doble - Descafeinado",
-                        info:"Para disfrutar sentado leyendo un libro",
+                        info:"Para disfrutar de tu tiempo libre",
                         foto:"./img/cafeSalon.jpg",
                         linkCompra:"#"
                     },
@@ -58,7 +58,7 @@ function opcionSuaveDescafeinado(){
                     {
                         cafeSeleccionado: "Bolsa de 1/4 molido - Descafeinado",
                         info:"Listo para preparar (para cafeteras con filtro de tela) ",
-                        foto:"./img/cafePaquete.jpg",
+                        foto:"./img/descafeinadoPaquete.jpg",
                         linkCompra:"#"
                     }
                 ];
@@ -80,7 +80,7 @@ function opcionSuaveDescafeinado(){
     }
 }
 
-function opcionSuaveColombia(){
+ function opcionSuaveColombia(){
     let op = prompt(`Como le preparamos su cafe?:
         1: Para llevar
         2: Para tomar en el local
@@ -116,7 +116,7 @@ function opcionSuaveColombia(){
                 const galeriaCafeSuaveC2 = [
                     {
                         cafeSeleccionado: "Cafe doble - Colombia",
-                        info:"Para disfrutar sentado leyendo un libro",
+                        info:"Para disfrutar de tu tiempo libre",
                         foto:"./img/cafeSalon.jpg",
                         linkCompra:"#"
                     },
@@ -139,7 +139,7 @@ function opcionSuaveColombia(){
                     {
                         cafeSeleccionado: "Bolsa de 1/4 molido - Colombia",
                         info:"Listo para preparar (para cafeteras con filtro de tela) ",
-                        foto:"./img/cafePaquete.jpg",
+                        foto:"./img/colombiaPaquete.jpg",
                         linkCompra:"#"
                     }
                 ];
@@ -184,6 +184,83 @@ function seleccionarVariedadSuave(){
 }
 
 //Intensidad Intermedio
+function opcionIntermedioBrasil(){
+    let op = prompt(`Como le preparamos su cafe?:
+    1: Para llevar
+    2: Para tomar en el local
+    3: Paquete de 1/4 molido
+    
+    Ingrese 1, 2 o 3.`);
+    switch(op){
+        case "1":
+            let opcionBrasil = Number;
+
+            const galeriaCafeIntermedioB1 = [
+                {
+                    cafeSeleccionado: "Vaso TakeAway - Brasil",
+                    info:"Ideal para tomar en el camino",
+                    foto:"./img/cafeTakeAway.jpg",
+                    linkCompra:"#"
+                }
+            ];
+
+            const indiceGaleriaCafeB1 = Math.floor(Math.random()*galeriaCafeIntermedioB1.length);
+
+            document.write(`<div class="card">
+            <img src=${galeriaCafeIntermedioB1[indiceGaleriaCafeB1].foto} alt=${galeriaCafeIntermedioB1[indiceGaleriaCafeB1].cafeSeleccionado}>
+            <h2>${galeriaCafeIntermedioB1[indiceGaleriaCafeB1].cafeSeleccionado}</h2>
+            <p class="card-text">${galeriaCafeIntermedioB1[indiceGaleriaCafeB1].info}</p>
+            <a href="${galeriaCafeIntermedioB1[indiceGaleriaCafeB1].linkCompra}" class="btn btn-primary">Pagar</a>
+            </div>`)
+
+        break;
+        case "2":
+            let opcionBrasil2 = Number;
+
+            const galeriaCafeIntermedioB2 = [
+                {
+                    cafeSeleccionado: "Cafe doble - Brasil",
+                    info:"Para disfrutar de tu tiempo libre",
+                        foto:"./img/cafeSalon.jpg",
+                        linkCompra:"#"
+                }
+            ];
+
+            const indiceGaleriaCafeB2 = Math.floor(Math.random()*galeriaCafeIntermedioB2.length);
+
+            document.write(`<div class="card">
+            <img src=${galeriaCafeIntermedioB2[indiceGaleriaCafeB2].foto} alt=${galeriaCafeIntermedioB2[indiceGaleriaCafeB2].cafeSeleccionado}>
+            <h2>${galeriaCafeIntermedioB2[indiceGaleriaCafeB2].cafeSeleccionado}</h2>
+            <p class="card-text">${galeriaCafeIntermedioB2[indiceGaleriaCafeB2].info}</p>
+            <a href="${galeriaCafeIntermedioB2[indiceGaleriaCafeB2].linkCompra}" class="btn btn-primary">Pagar</a>
+            </div>`)
+
+        break;
+        case "3":
+            let opcionBrasil3 = Number;
+
+            const galeriaCafeIntermedioB3 = [
+                {
+                    cafeSeleccionado: "Bolsa de 1/4 molido - Brasil",
+                        info:"Listo para preparar (para cafeteras con filtro de tela) ",
+                        foto:"./img/brasilPaquete.jpg",
+                        linkCompra:"#"
+                }
+            ];
+
+            const indiceGaleriaCafeB3 = Math.floor(Math.random()*galeriaCafeIntermedioB3.length);
+
+            document.write(`<div class="card">
+            <img src=${galeriaCafeIntermedioB3[indiceGaleriaCafeB3].foto} alt=${galeriaCafeIntermedioB3[indiceGaleriaCafeB3].cafeSeleccionado}>
+            <h2>${galeriaCafeIntermedioB3[indiceGaleriaCafeB3].cafeSeleccionado}</h2>
+            <p class="card-text">${galeriaCafeIntermedioB3[indiceGaleriaCafeB3].info}</p>
+            <a href="${galeriaCafeIntermedioB3[indiceGaleriaCafeB3].linkCompra}" class="btn btn-primary">Pagar</a>
+            </div>`)
+
+        break;
+    }
+}
+
 function opcionIntermedioSelecto(){
     let op = prompt(`Como le preparamos su cafe?:
     1: Para llevar
@@ -220,7 +297,7 @@ function opcionIntermedioSelecto(){
             const galeriaCafeIntermedioS2 = [
                 {
                     cafeSeleccionado: "Cafe doble - Selecto",
-                    info:"Para disfrutar sentado leyendo un libro",
+                    info:"Para disfrutar de tu tiempo libre",
                     foto:"./img/cafeSalon.jpg",
                     linkCompra:"#"
                 },
@@ -243,7 +320,7 @@ function opcionIntermedioSelecto(){
                 {
                     cafeSeleccionado: "Bolsa de 1/4 molido - Selecto",
                     info:"Listo para preparar (para cafeteras con filtro de tela) ",
-                    foto:"./img/selecto.jpg",
+                    foto:"./img/selectoPaquete.jpg",
                     linkCompra:"#"
                 }
             ];
@@ -260,7 +337,7 @@ function opcionIntermedioSelecto(){
         break;
         default:
             alert("Elije una opcion valida");
-            opcionIntermedioSelecto();
+            opcionIntermedioBrasil();
     }
 }
 
@@ -285,6 +362,188 @@ function seleccionarVariedadIntermedio(){
     }
 }
 
+//Intensidad Fuerte
+function seleccionarVariedadFuerte(){
+    let op = prompt(`Los cafes fuertes que tenemos son:
+    1: Moka
+    2: Italiano.
+    
+    Ingrese 1 o 2.`)
+    switch(op){
+        case "1":
+            let variedadFuerteMoka = Number;
+            opcionFuerteMoka();
+            break;
+        case "2":
+            let variedadFuerteItaliano = Number;
+            opcionFuerteItaliano();
+            break;
+            default:
+                alert('Elije una opcion valida');
+                seleccionarVariedadFuerte();
+    }
+}
+
+function opcionFuerteMoka(){
+    let op = prompt(`Como le preparamos su cafe?:
+    1: Para llevar
+    2: Para tomar en el local
+    3: Paquete de 1/4 molido
+    
+    Ingrese 1, 2 o 3.`);
+    switch(op){
+        case "1":
+            let opcionMoka = Number;
+
+            const galeriaCafeFuerteM1 = [
+                {
+                    cafeSeleccionado: "Vaso TakeAway - Moka",
+                    info:"Ideal para tomar en el camino",
+                    foto:"./img/cafeTakeAway.jpg",
+                    linkCompra:"#"
+                }
+            ];
+
+            const indiceGaleriaCafeM1 = Math.floor(Math.random()*galeriaCafeFuerteM1.length);
+
+            document.write(`<div class="card">
+            <img src=${galeriaCafeFuerteM1[indiceGaleriaCafeM1].foto} alt=${galeriaCafeFuerteM1[indiceGaleriaCafeM1].cafeSeleccionado}>
+            <h2>${galeriaCafeFuerteM1[indiceGaleriaCafeM1].cafeSeleccionado}</h2>
+            <p class="card-text">${galeriaCafeFuerteM1[indiceGaleriaCafeM1].info}</p>
+            <a href="${galeriaCafeFuerteM1[indiceGaleriaCafeM1].linkCompra}" class="btn btn-primary">Pagar</a>
+            </div>`)
+
+        break;
+        case "2":
+            let opcionMoka2 = Number;
+
+            const galeriaCafeFuerteM2 = [
+                {
+                    cafeSeleccionado: "Cafe doble - Moka",
+                    info:"Para disfrutar de tu tiempo libre",
+                    foto:"./img/cafeSalon.jpg",
+                    linkCompra:"#"
+                }
+            ];
+
+            const indiceGaleriaCafeM2 = Math.floor(Math.random()*galeriaCafeFuerteM2.length);
+
+            document.write(`<div class="card">
+            <img src=${galeriaCafeFuerteM2[indiceGaleriaCafeM2].foto} alt=${galeriaCafeFuerteM2[indiceGaleriaCafeM2].cafeSeleccionado}>
+            <h2>${galeriaCafeFuerteM2[indiceGaleriaCafeM2].cafeSeleccionado}</h2>
+            <p class="card-text">${galeriaCafeFuerteM2[indiceGaleriaCafeM2].info}</p>
+            <a href="${galeriaCafeFuerteM2[indiceGaleriaCafeM2].linkCompra}" class="btn btn-primary">Pagar</a>
+            </div>`)
+
+        break;
+        case "3":
+            let opcionMoka3 = Number;
+
+            const galeriaCafeFuerteM3 = [
+                {
+                    cafeSeleccionado: "Bolsa de 1/4 molido - Moka",
+                    info:"Listo para preparar (para cafeteras con filtro de tela) ",
+                    foto:"./img/mokaPaquete.jpg",
+                    linkCompra:"#"
+                }
+            ];
+
+            const indiceGaleriaCafeM3 = Math.floor(Math.random()*galeriaCafeFuerteM3.length);
+
+            document.write(`<div class="card">
+            <img src=${galeriaCafeFuerteM3[indiceGaleriaCafeM3].foto} alt=${galeriaCafeFuerteM3[indiceGaleriaCafeM3].cafeSeleccionado}>
+            <h2>${galeriaCafeFuerteM3[indiceGaleriaCafeM3].cafeSeleccionado}</h2>
+            <p class="card-text">${galeriaCafeFuerteM3[indiceGaleriaCafeM3].info}</p>
+            <a href="${galeriaCafeFuerteM3[indiceGaleriaCafeM3].linkCompra}" class="btn btn-primary">Pagar</a>
+            </div>`)
+
+        break;
+        
+    }
+}
+
+function opcionFuerteItaliano(){
+    let op = prompt(`Como le preparamos su cafe?:
+    1: Para llevar
+    2: Para tomar en el local
+    3: Paquete de 1/4 molido
+    
+    Ingrese 1, 2 o 3.`);
+    switch(op){
+        case "1":
+            let opcionItaliano = Number;
+
+            const galeriaCafeFuerteI1 = [
+                {
+                    cafeSeleccionado: "Vaso TakeAway - Italiano",
+                    info:"Ideal para tomar en el camino",
+                    foto:"./img/cafeTakeAway.jpg",
+                    linkCompra:"#"
+                }
+            ];
+
+            const indiceGaleriaCafeI1 = Math.floor(Math.random()*galeriaCafeFuerteI1.length);
+
+            document.write(`<div class="card">
+            <img src=${galeriaCafeFuerteI1[indiceGaleriaCafeI1].foto} alt=${galeriaCafeFuerteI1[indiceGaleriaCafeI1].cafeSeleccionado}>
+            <h2>${galeriaCafeFuerteI1[indiceGaleriaCafeI1].cafeSeleccionado}</h2>
+            <p class="card-text">${galeriaCafeFuerteI1[indiceGaleriaCafeI1].info}</p>
+            <a href="${galeriaCafeFuerteI1[indiceGaleriaCafeI1].linkCompra}" class="btn btn-primary">Pagar</a>
+            </div>`)
+
+        break;
+        case "2":
+            let opcionItaliano2 = Number;
+
+            const galeriaCafeFuerteI2 = [
+                {
+                    cafeSeleccionado: "Cafe doble - Italiano",
+                    info:"Para disfrutar de tu tiempo libre",
+                    foto:"./img/cafeSalon.jpg",
+                    linkCompra:"#"
+                }
+            ];
+
+            const indiceGaleriaCafeI2 = Math.floor(Math.random()*galeriaCafeFuerteI2.length);
+
+            document.write(`<div class="card">
+            <img src=${galeriaCafeFuerteI2[indiceGaleriaCafeI2].foto} alt=${galeriaCafeFuerteI2[indiceGaleriaCafeI2].cafeSeleccionado}>
+            <h2>${galeriaCafeFuerteI2[indiceGaleriaCafeI2].cafeSeleccionado}</h2>
+            <p class="card-text">${galeriaCafeFuerteI2[indiceGaleriaCafeI2].info}</p>
+            <a href="${galeriaCafeFuerteI2[indiceGaleriaCafeI2].linkCompra}" class="btn btn-primary">Pagar</a>
+            </div>`)
+
+        break;
+        case "3":
+            let opcionItaliano3 = Number;
+
+            const galeriaCafeFuerteI3 = [
+                {
+                    cafeSeleccionado: "Bolsa de 1/4 molido - Italiano",
+                    info:"Listo para preparar (para cafeteras con filtro de tela) ",
+                    foto:"./img/italianoPaquete.jpg",
+                    linkCompra:"#"
+                }
+            ];
+
+            const indiceGaleriaCafeI3 = Math.floor(Math.random()*galeriaCafeFuerteI3.length);
+
+            document.write(`<div class="card">
+            <img src=${galeriaCafeFuerteI3[indiceGaleriaCafeI3].foto} alt=${galeriaCafeFuerteI3[indiceGaleriaCafeI3].cafeSeleccionado}>
+            <h2>${galeriaCafeFuerteI3[indiceGaleriaCafeI3].cafeSeleccionado}</h2>
+            <p class="card-text">${galeriaCafeFuerteI3[indiceGaleriaCafeI3].info}</p>
+            <a href="${galeriaCafeFuerteI3[indiceGaleriaCafeI3].linkCompra}" class="btn btn-primary">Pagar</a>
+            </div>`)
+
+        break;
+        default:
+            alert('Elije una opcion valida');
+            opcionFuerteItaliano();
+        
+    }
+}
+
+//Seleccion de intensidad del cafe
 function seleccionarIntensidad(){
     
     let op = prompt(`Que tipo de café le gusta?
@@ -311,7 +570,7 @@ function seleccionarIntensidad(){
             seleccionarIntensidad();
     }
 }
-
+//respuesta del usuario
 function elegirRespuesta(respuesta){
     if(respuesta == 'si'){
         seleccionarIntensidad();
@@ -319,17 +578,17 @@ function elegirRespuesta(respuesta){
         alert("Que tenga un buen dia!");
     }
 }
-
+//opciones si - no
 function tomarDecision(bienvenida){
     if(bienvenida){
         let respuesta = prompt("¿Querés una recomendacion de Café?  SI / NO").toLowerCase();
         elegirRespuesta(respuesta);
     }
 }
-
+//saludo
 function iniciarRecomendacion(){
     let bienvenida = confirm("--------Bienvenido a CafeMartinez Online--------");
     tomarDecision(bienvenida)
 }
-
+//init
 iniciarRecomendacion();
